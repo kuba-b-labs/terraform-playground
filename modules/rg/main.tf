f7036d5699eb2330f7f82ec7e5433abe70b950f4
Kuba123pl <jakub.borowiec29@gmail.com>
2026-03-12 09:11:38 +0100

    add test pipeline

---
+++ modules/rg/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "default" {
+  location = "northcentralus"
+  name = "testrg"
+}
---
+++ rg/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = ">=4.1.0, <4.40.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  resource_provider_registrations = "none"
+}
